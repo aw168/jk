03259ef1dffb8fee05d37344fa512757db215557
aw168 <965868345@qq.com>
2025-01-24 00:38:55 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,6 +1,6 @@
 const pageConfig = {
   // Title for your status page
-  title: "hxd Status Page",
+  title: "监控平台",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://awbk.us.kg', label: '博客' },
@@ -11,25 +11,230 @@ const pageConfig = {
 const workerConfig = {
   kvWriteCooldownMinutes: 3,
   monitors: [
-    {
-      id: 'google_monitor',
-      name: '1',
-      method: 'GET',
-      target: 'https://qq965868345.serv00.net',
-        tooltip: 'This is a tooltip for this monitor',
-  // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+{
+  id: 'google_monitor',
+  name: '1',
+  method: 'GET',
+  target: 'https://qq965868345.serv00.net',
+  tooltip: 'This is a tooltip for this monitor',
   statusPageLink: 'https://qq965868345.serv00.net',
-    },
+},
 
- {
-      id: 'google_monitor',
-      name: '2',
-      method: 'GET',
-      target: 'https://cueiringhih.serv00.net/',
-     tooltip: 'This is a tooltip for this monitor',
-  // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+{
+  id: 'google_monitor',
+  name: '2',
+  method: 'GET',
+  target: 'https://cueiringhih.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
   statusPageLink: 'https://cueiringhih.serv00.net/',
-    },
+},
+
+{
+  id: 'google_monitor',
+  name: '3',
+  method: 'GET',
+  target: 'https://fafaweifaf.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://fafaweifaf.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '4',
+  method: 'GET',
+  target: 'https://viogp.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://viogp.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '5',
+  method: 'GET',
+  target: 'https://dalugela.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://dalugela.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '6',
+  method: 'GET',
+  target: 'https://eusouzblg.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://eusouzblg.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '7',
+  method: 'GET',
+  target: 'https://qgikipr.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://qgikipr.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '8',
+  method: 'GET',
+  target: 'https://uhif.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://uhif.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '9',
+  method: 'GET',
+  target: 'https://sjbsbjio.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://sjbsbjio.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '10',
+  method: 'GET',
+  target: 'https://dtocxxltbimh.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://dtocxxltbimh.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '11',
+  method: 'GET',
+  target: 'https://r3jgmud24lbird.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://r3jgmud24lbird.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '12',
+  method: 'GET',
+  target: 'https://fhdqfxw.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://fhdqfxw.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '13',
+  method: 'GET',
+  target: 'https://lqcxsgcisafnn6.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://lqcxsgcisafnn6.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '14',
+  method: 'GET',
+  target: 'https://eqdinjzp.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://eqdinjzp.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '15',
+  method: 'GET',
+  target: 'https://yklwkmbjvg.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://yklwkmbjvg.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '16',
+  method: 'GET',
+  target: 'https://yo54uaimwzvyhr.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://yo54uaimwzvyhr.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '17',
+  method: 'GET',
+  target: 'https://rutgravyg.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://rutgravyg.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '18',
+  method: 'GET',
+  target: 'https://getcfa.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://getcfa.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '19',
+  method: 'GET',
+  target: 'https://ucwljw.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://ucwljw.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '20',
+  method: 'GET',
+  target: 'https://tmlwjbb.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://tmlwjbb.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '21',
+  method: 'GET',
+  target: 'https://ufqmxmzr.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://ufqmxmzr.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '22',
+  method: 'GET',
+  target: 'https://gck7ampcqjg.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://gck7ampcqjg.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '23',
+  method: 'GET',
+  target: 'https://bnmqwerty.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://bnmqwerty.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '24',
+  method: 'GET',
+  target: 'https://xgi.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://xgi.serv00.net/',
+},
+
+{
+  id: 'google_monitor',
+  name: '25',
+  method: 'GET',
+  target: 'https://faweia.serv00.net/',
+  tooltip: 'This is a tooltip for this monitor',
+  statusPageLink: 'https://faweia.serv00.net/',
+},
 
      {
       id: 'google_monitor',
